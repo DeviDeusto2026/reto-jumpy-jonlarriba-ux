c59dc33d83688b4fe2ca52715167437d70106526
Jon Larriba <jon.larriba@opendeusto.es>
2026-04-25 17:45:33 +0200

    Going outside of camera's visibility triggers an event that restarts game

---
+++ Assets/Scripts/Autoscroll.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Autoscroll : MonoBehaviour
+{
+    public float speed = 1;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = this.transform.position + Vector3.up * speed;
+    }
+}
--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -1,8 +1,13 @@
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
-    public Transform spawn;
+    [SerializeField] 
+    private GameObject target;
+    [SerializeField]
+    private Camera cam;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,14 +18,36 @@ public class LogicScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Void"))
+        if (isVisible())
         {
-            gameObject.transform.position = spawn.position;
+            Debug.Log("In Camera");
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            //https://youtu.be/kn7rL8Jeexo?si=8dglj940CHcyF67s
+            Debug.Log("Outside Camera");
         }
     }
+
+    private bool isVisible()
+    {
+        //Get every single plane of the camera frustrum
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(cam);
+        //Return true if the object is inside every single plane of the frustrum
+        return planes.All(plane => plane.GetDistanceToPoint(target.transform.position) >= 0);
+        //https://youtu.be/38BtpQWEct4?si=TkqhBBqhTodTv0j- 
+    }
+/**
+ * Non lambda alternative:
+ * foreach (var plane in planes){
+ *      if (plane.GetDistanceToPoint(target.transform.position) >= 0){
+ *          return false;
+ *      }
+ *      
+ * }
+ * return true;
+}
+**/
+
 }
